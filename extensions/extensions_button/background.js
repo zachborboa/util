c1288a6f5165bd0be736315c1cedf11cac80c32b
Zach Borboa <zachborboa@gmail.com>
2015-11-12 12:25:39 -0800

    Add Extensions Button extension

---
+++ extensions/extensions_button/background.js
@@ -0,0 +1,16 @@
+chrome.browserAction.onClicked.addListener(function( tab ) {
+    chrome.tabs.query({ 'url': 'chrome://extensions/' }, function ( tabs ) {
+        for ( var i = 0, tab; i < tabs.length; i++ ) {
+            chrome.tabs.update( tabs[ i ].id, {
+                'active': true
+            });
+            chrome.windows.update( tabs[ i ].windowId, {
+                'focused': true
+            });
+            return;
+        }
+        chrome.tabs.create({
+            'url': 'chrome://extensions/'
+        });
+    });
+});
