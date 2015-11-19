89bb115d716860e343e472cfa103434a15d1b50a
Zach Borboa <zachborboa@gmail.com>
2015-11-18 23:20:19 -0800

    Standardize location of js files

--- extensions/extensions_button/js/background.js
+++ extensions/extensions_button/js/background.js
@@ -2,15 +2,15 @@ chrome.browserAction.onClicked.addListener(function( tab ) {
     chrome.tabs.query({ 'url': 'chrome://extensions/' }, function ( tabs ) {
         for ( var i = 0, tab; i < tabs.length; i++ ) {
             chrome.tabs.update( tabs[ i ].id, {
-                'active': true
+                'active': true,
             });
             chrome.windows.update( tabs[ i ].windowId, {
-                'focused': true
+                'focused': true,
             });
             return;
         }
         chrome.tabs.create({
-            'url': 'chrome://extensions/'
+            'url': 'chrome://extensions/',
         });
     });
 });
