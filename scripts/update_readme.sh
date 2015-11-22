a77c4db6d26b25a48da3da0182bc4fdc93d5cb23
Zach Borboa <zachborboa@gmail.com>
2015-11-21 23:19:39 -0800

    Update readme; Add readme update script

---
+++ scripts/update_readme.sh
@@ -0,0 +1,5 @@
+readme="$(dirname $(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd))/README.md"
+echo -e "# Extensions\n" > "${readme}"
+#echo $(find . -iname "manifest.json" -exec php -r '$path = "{}"; $data = json_decode(file_get_contents($path)); echo "### " . $data->name . "\n\n" . $data->description . "\n\n";' \;) >> "${readme}"
+echo -e "$(find . -iname "manifest.json" -exec php -r '$path = "{}"; $data = json_decode(file_get_contents($path)); echo
+"### " . $data->name . "\n\n" . $data->description . "\n\n";' \;)" >> "${readme}"
