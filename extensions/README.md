d2f5680af07bf5182beb3a730c4da5a59dc42144
Zach Borboa <zachborboa@gmail.com>
2015-12-02 01:03:25 -0800

    Generate an extensions readme when running readme script

---
+++ extensions/README.md
@@ -0,0 +1,21 @@
+## Extensions
+
+### Always Select
+
+Always Select prevents sites from disabling user selection via css.
+
+### Downloads Button
+
+Provides quick icon access to your Chrome downloads.
+
+### Extensions Button
+
+Provides quick icon access to your Chrome extensions.
+
+### Pin Tab
+
+Provides quick access to pin and unpin your tabs.
+
+### Weather Homepage
+
+Give the weather search page a clean look.
--- scripts/update_readme.sh
+++ scripts/update_readme.sh
@@ -1,5 +1,13 @@
-readme="$(dirname $(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd))/README.md"
+root="$(dirname $(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd))/"
+cd "${root}"
+
+# Generate readme for extensions.
+cd "extensions"
+echo -e "## Extensions\n" > "README.md"
+echo -e "$(find . -iname "manifest.json" -exec php -r '$path = "{}"; $data = json_decode(file_get_contents($path)); echo "### " . $data->name . "\n\n" . $data->description . "\n\n";' \;)" >> "README.md"
+cd "${root}"
+
+# Generate main readme.
+readme="${root}/README.md"
 echo -e "# Util\n" > "${readme}"
-echo -e "## Extensions\n" >> "${readme}"
-echo -e "$(find . -iname "manifest.json" -exec php -r '$path = "{}"; $data = json_decode(file_get_contents($path)); echo
-"### " . $data->name . "\n\n" . $data->description . "\n\n";' \;)" >> "${readme}"
+cat "extensions/README.md" >> "${readme}"
