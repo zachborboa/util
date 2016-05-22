937f700b6bd9974b5e0ce1fdbadd7a504210fc8a
Zach Borboa <zachborboa@gmail.com>
2016-05-22 11:12:44 -0700

    Display file when cleaning

--- extensions/scripts/clean_up_json_file.py
+++ extensions/scripts/clean_up_json_file.py
@@ -5,10 +5,11 @@ import sys
 
 
 file_path = sys.argv[1]
+print file_path
 with open(file_path, 'r') as f:
     data = f.read()
 
 json_data = json.loads(data)
 pretty_json_data = json.dumps(json_data, indent=2, sort_keys=True, separators = (',', ': '))
 with open(file_path, 'w') as f:
-    f.write(pretty_json_data)
+    f.write('{}'.format(pretty_json_data))
