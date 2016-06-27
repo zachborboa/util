f3deeca7c2b79d3fd423b53c6936c9720e81c29b
Zach Borboa <zachborboa@gmail.com>
2016-06-27 09:17:08 -0700

    Add option to add additional url patterns

--- extensions/keep_tabs_open/js/options.js
+++ extensions/keep_tabs_open/js/options.js
@@ -1,1 +1,9 @@
 console.info('options.js');
+
+$('#keep-open-add-another-url').click(function(event) {
+    event.preventDefault();
+    $('#keep-open-url-pattern')
+        .clone()
+        .removeAttr('id')
+        .appendTo('.keep-open-url-pattern-wrapper');
+});
