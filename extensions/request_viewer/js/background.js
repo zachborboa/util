1e3608424628e02c8217721f7131258698b7fd89
Zach Borboa <zachborboa@gmail.com>
2016-09-08 15:33:39 -0700

    Add request header logging

--- extensions/request_viewer/js/background.js
+++ extensions/request_viewer/js/background.js
@@ -1,9 +1,27 @@
 chrome.webRequest.onBeforeRequest.addListener(
-    function ( details ) {
-        console.log( details.requestId, details.type, details.method, details.url );
+    function(details) {
+        console.info(details.requestId, details.type, details.method, details.url);
     },
     {
         urls: ["<all_urls>"],
         types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
     }
+);
+
+chrome.webRequest.onBeforeSendHeaders.addListener(
+    function(details) {
+        for ( var i = 0; i < details.requestHeaders.length; ++i ) {
+            var requestHeader = details.requestHeaders[i];
+            console.log('request header:', requestHeader.name, requestHeader.value);
+        }
+
+        return {
+            requestHeaders: details.requestHeaders,
+        };
+    },
+    {
+        urls: ["<all_urls>"],
+        types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"],
+    },
+    ["blocking", "requestHeaders"]
 );
