51714ca1dc4904ba49ed332a59d4206561cb31c9
Zach Borboa <zachborboa@gmail.com>
2018-02-19 20:39:53 -0800

    Add buttons to calendar event bubble

--- extensions/google_calendar_workflow/js/content_script.js
+++ extensions/google_calendar_workflow/js/content_script.js
@@ -13,6 +13,86 @@ function insertAfter(newNode, referenceNode) {
     referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
 }
 
+function insertButtonsAfter(referenceNode, onclickAction) {
+    var insertAfterTarget = referenceNode;
+    var buttonLabels = [
+        // buttonLabel, eventTitlePrefix.
+        ['DONE', '✓'],
+        ['NOPE', '✗'],
+        ['OKAY', '▣'],
+        ['AWESOME', 'ツ'],
+    ];
+    for ( var i in buttonLabels ) {
+        var buttonLabel = buttonLabels[i][0];
+        console.log('buttonLabel:', buttonLabel);
+
+        var eventTitlePrefix = buttonLabels[i][1];
+        console.log('eventTitlePrefix:', eventTitlePrefix);
+
+        var button = document.createElement('button');
+        button.innerText = buttonLabel;
+
+        (function(myButton, myEventTitlePrefix) {
+            myButton.onclick = function() {
+                console.log('myButton.onclick');
+                onclickAction(myButton, myEventTitlePrefix);
+            };
+        }(button, eventTitlePrefix));
+
+        console.log('inserting button', button, 'after', insertAfterTarget);
+        insertAfter(button, insertAfterTarget);
+        insertAfterTarget = button;
+    }
+}
+
+function clickButton(clickedData) {
+    console.group('clickButton');
+    console.log('clickedData:', clickedData);
+    console.log('button clicked');
+
+    // Today.
+    var date = new Date();
+    var monthName = date.toLocaleString('en-us', { 'month': 'short' });
+    var todayFormattedDate = monthName + ' ' + date.getDate() + ', ' + date.getFullYear();
+
+    // Event date.
+    var startDateInput = document.querySelector('[aria-label="Start date"]');
+    var endDateInput = document.querySelector('[aria-label="End date"]');
+    var eventDate = startDateInput.value;
+    if (endDateInput.value !== startDateInput.value) {
+        eventDate += ' - ' + endDateInput.value;
+    }
+    console.log('eventDate:', eventDate);
+
+    var eventTitle = document.querySelector('[aria-label="Title"]');
+    var calendarEventTitle = eventTitle.value;
+    console.log('before calendarEventTitle:', calendarEventTitle);
+
+    // Remove leading number (e.g. "1. " in "1. My Calendar Event").
+    calendarEventTitle = calendarEventTitle.replace(/^\d+\. /, '');
+    // Remove leading ~ character.
+    calendarEventTitle = calendarEventTitle.replace(/^~ /, '');
+
+    console.log(' after calendarEventTitle:', calendarEventTitle);
+
+    // "✓ My Event; Dec 31, 2015; event date: Jan 1, 2016"
+    var eventTitlePrefix = clickedData['eventTitlePrefix'];
+    var newCalendarEventTitle =
+        eventTitlePrefix + ' ' + calendarEventTitle + ';' +
+        ' ' + todayFormattedDate + ';' +
+        ' event date: ' + eventDate;
+    console.log('newCalendarEventTitle:', newCalendarEventTitle);
+    eventTitle.value = newCalendarEventTitle;
+
+    dispatchEvent(eventTitle, 'input');
+
+    var saveButton = document.querySelector('[aria-label="Save"]');
+    saveButton.click();
+
+    console.groupEnd();
+}
+
+var buttonClickedData;
 (function(){
     var pathname = window.location.pathname;
     setInterval(function() {
@@ -22,77 +102,68 @@ function insertAfter(newNode, referenceNode) {
             if (pathname.match(/^\/calendar\/r\/eventedit\//)) {
                 console.log('on event edit page');
 
-                // Add action buttons to calendar event edit page.
-                console.log('adding buttons');
-                var saveButton = document.querySelector('[aria-label="Save"]');
-                var insertAfterTarget = saveButton;
-                var buttonLabels = [
-                    // buttonLabel, eventTitlePrefix.
-                    ['DONE', '✓'],
-                    ['NOPE', '✗'],
-                    ['OKAY', '▣'],
-                    ['AWESOME', 'ツ'],
-                ];
-                for ( var i in buttonLabels ) {
-                    var buttonLabel = buttonLabels[i][0];
-                    console.log('buttonLabel:', buttonLabel);
-
-                    var eventTitlePrefix = buttonLabels[i][1];
-                    console.log('eventTitlePrefix:', eventTitlePrefix);
-
-                    var button = document.createElement('button');
-                    button.innerText = buttonLabel;
-
-                    (function(myEventTitlePrefix, myButton) {
-                        myButton.onclick = function() {
-                            console.log('button clicked');
-
-                            // Today.
-                            var date = new Date();
-                            var monthName = date.toLocaleString('en-us', { 'month': 'short' });
-                            var todayFormattedDate = monthName + ' ' + date.getDate() + ', ' + date.getFullYear();
-
-                            // Event date.
-                            var startDateInput = document.querySelector('[aria-label="Start date"]');
-                            var endDateInput = document.querySelector('[aria-label="End date"]');
-                            var eventDate = startDateInput.value;
-                            if (endDateInput.value !== startDateInput.value) {
-                                eventDate += ' - ' + endDateInput.value;
-                            }
-                            console.log('eventDate:', eventDate);
-
-                            var eventTitle = document.querySelector('[aria-label="Title"]');
-                            var calendarEventTitle = eventTitle.value;
-                            console.log('before calendarEventTitle:', calendarEventTitle);
-
-                            // Remove leading number (e.g. "1. " in "1. My Calendar Event").
-                            calendarEventTitle = calendarEventTitle.replace(/^\d+\. /, '');
-                            // Remove leading ~ character.
-                            calendarEventTitle = calendarEventTitle.replace(/^~ /, '');
-
-                            console.log(' after calendarEventTitle:', calendarEventTitle);
-
-                            // "✓ My Event; Dec 31, 2015; event date: Jan 1, 2016"
-                            var newCalendarEventTitle =
-                                myEventTitlePrefix + ' ' + calendarEventTitle + ';' +
-                                ' ' + todayFormattedDate + ';' +
-                                ' event date: ' + eventDate;
-                            console.log('newCalendarEventTitle:', newCalendarEventTitle);
-                            eventTitle.value = newCalendarEventTitle;
-
-                            dispatchEvent(eventTitle, 'input');
-                            saveButton.click();
+                if (buttonClickedData) {
+                    clickButton(buttonClickedData);
+                    buttonClickedData = null;
+                } else {
+                    // Add action buttons to calendar event edit page.
+                    console.log('adding buttons');
+                    var saveButton = document.querySelector('[aria-label="Save"]');
+                    var onclickAction = function(myButton, myEventTitlePrefix) {
+                        buttonClickedData = {
+                            'button': myButton,
+                            'eventTitlePrefix': myEventTitlePrefix,
                         };
-                    }(eventTitlePrefix, button));
-
-                    console.log('inserting button', button, 'after', insertAfterTarget);
-                    insertAfter(button, insertAfterTarget);
-                    insertAfterTarget = button;
+                        clickButton(buttonClickedData);
+                        buttonClickedData = null;
+                    };
+                    insertButtonsAfter(saveButton, onclickAction);
                 }
             }
         }
     }, 1000);
 })();
+
+document.onclick = function(event) {
+    console.group('element clicked');
+    console.log('element clicked:', event.target);
+
+    var target = event.target;
+    var eventIdFound = false;
+    for (var i = 0; i < 5; i++) {
+        // Ignore event bubble.
+        if (target.getAttribute('id') === 'xDetDlg') {
+            break;
+        } else if (target.getAttribute('data-eventid')) {
+            eventIdFound = true;
+            break;
+        }
+        target = target.parentElement;
+    }
+    console.log('target:', target);
+    if (eventIdFound) {
+        var eventId = target.getAttribute('data-eventid');
+        console.log('event id found:', eventId);
+        setTimeout(function() {
+            var eventBubble = document.querySelector('#xDetDlg[data-eventid="' + eventId + '"]');
+            console.log('eventBubble:', eventBubble);
+
+            var lastEventBubbleMetaItem = document.querySelector('#xDtlDlgCt > div:last-child');
+            var onclickAction = function(myButton, myEventTitlePrefix) {
+                buttonClickedData = {
+                    'button': myButton,
+                    'eventTitlePrefix': myEventTitlePrefix,
+                };
+
+                var editEventButton = document.querySelector('[aria-label="Edit event"]');
+                console.log('editEventButton:', editEventButton);
+                editEventButton.click();
+            };
+            insertButtonsAfter(lastEventBubbleMetaItem, onclickAction);
+        }, 100);
+    }
+    console.groupEnd();
+};
 
 window.onhashchange = function() {
     console.log('hash changed', window.location.hash);
