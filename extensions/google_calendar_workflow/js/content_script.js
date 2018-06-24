a7ff79e03cecb93929a02b8213f929bfd91a2362
Zach Borboa <zachborboa@gmail.com>
2018-06-23 23:40:45 -0700

    Add debug flag to control logging to console; Disable logging

--- extensions/google_calendar_workflow/js/content_script.js
+++ extensions/google_calendar_workflow/js/content_script.js
@@ -1,4 +1,5 @@
 const PRIORITY_EVENT_TITLE_PREFIXES = ['1.', '2.', '3.', '4.', '5.', null];
+const DEBUG = false;
 
 function dispatchEvent(obj, event) {
     var evt = new Event(
@@ -32,13 +33,13 @@ function insertButtons(referenceNode, onclickAction, where, alternateReferenceNo
     ];
     for (var i in buttonLabels) {
         var buttonLabel = buttonLabels[i][0];
-        console.log('buttonLabel:', buttonLabel);
+        DEBUG && console.log('buttonLabel:', buttonLabel);
 
         var eventTitlePrefix = buttonLabels[i][1];
-        console.log('eventTitlePrefix:', eventTitlePrefix);
+        DEBUG && console.log('eventTitlePrefix:', eventTitlePrefix);
 
         var buttonClassNames = buttonLabels[i][2];
-        console.log('buttonClassNames:', buttonClassNames);
+        DEBUG && console.log('buttonClassNames:', buttonClassNames);
 
         var button = document.createElement('button');
         button.innerText = buttonLabel;
@@ -46,12 +47,12 @@ function insertButtons(referenceNode, onclickAction, where, alternateReferenceNo
 
         (function(myButton, myEventTitlePrefix) {
             myButton.onclick = function() {
-                console.log('myButton.onclick');
+                DEBUG && console.log('myButton.onclick');
                 onclickAction(myButton, myEventTitlePrefix);
             };
         }(button, eventTitlePrefix));
 
-        console.log('inserting button', button, where, insertTarget);
+        DEBUG && console.log('inserting button', button, where, insertTarget);
         if (where === 'after') {
             insertAfter(button, insertTarget);
             insertTarget = button;
@@ -66,9 +67,9 @@ function insertButtons(referenceNode, onclickAction, where, alternateReferenceNo
 }
 
 function clickButton(clickedData) {
-    console.group('clickButton');
-    console.log('clickedData:', clickedData);
-    console.log('button clicked');
+    DEBUG && console.group('clickButton');
+    DEBUG && console.log('clickedData:', clickedData);
+    DEBUG && console.log('button clicked');
 
     // Today.
     var date = new Date();
@@ -82,11 +83,11 @@ function clickButton(clickedData) {
     if (endDateInput.value !== startDateInput.value) {
         eventDate += ' - ' + endDateInput.value;
     }
-    console.log('eventDate:', eventDate);
+    DEBUG && console.log('eventDate:', eventDate);
 
     var eventTitle = document.querySelector('[aria-label="Title"]');
     var calendarEventTitle = eventTitle.value;
-    console.log('before calendarEventTitle:', calendarEventTitle);
+    DEBUG && console.log('before calendarEventTitle:', calendarEventTitle);
 
     // Remove leading number (e.g. "1. " in "1. My Calendar Event").
     calendarEventTitle = calendarEventTitle.replace(/^\d+\. /, '');
@@ -95,7 +96,7 @@ function clickButton(clickedData) {
     // Remove leading ~ character.
     calendarEventTitle = calendarEventTitle.replace(/^~ /, '');
 
-    console.log(' after calendarEventTitle:', calendarEventTitle);
+    DEBUG && console.log(' after calendarEventTitle:', calendarEventTitle);
 
     // "✓ My Event; Dec 31, 2015; event date: Jan 1, 2016"
     // "1. My Event"
@@ -110,7 +111,7 @@ function clickButton(clickedData) {
                 ' event date: ' + eventDate;
         }
     }
-    console.log('newCalendarEventTitle:', newCalendarEventTitle);
+    DEBUG && console.log('newCalendarEventTitle:', newCalendarEventTitle);
     eventTitle.value = newCalendarEventTitle;
 
     dispatchEvent(eventTitle, 'input');
@@ -120,7 +121,7 @@ function clickButton(clickedData) {
         saveButton.click();
     }, 500);
 
-    console.groupEnd();
+    DEBUG && console.groupEnd();
 }
 
 var buttonClickedData;
@@ -128,17 +129,17 @@ var buttonClickedData;
     var pathname = window.location.pathname;
     setInterval(function() {
         if (pathname !== window.location.pathname) {
-            console.log('pathname changed');
+            DEBUG && console.log('pathname changed');
             pathname = window.location.pathname;
             if (pathname.match(/^\/calendar\/r\/eventedit\//)) {
-                console.log('on event edit page');
+                DEBUG && console.log('on event edit page');
 
                 if (buttonClickedData) {
                     clickButton(buttonClickedData);
                     buttonClickedData = null;
                 } else {
                     // Add action buttons to calendar event edit page.
-                    console.log('adding buttons');
+                    DEBUG && console.log('adding buttons');
                     var onclickAction = function(myButton, myEventTitlePrefix) {
                         buttonClickedData = {
                             'button': myButton,
@@ -166,8 +167,8 @@ var buttonClickedData;
 })();
 
 document.onclick = function(event) {
-    console.group('element clicked');
-    console.log('element clicked:', event.target);
+    DEBUG && console.group('element clicked');
+    DEBUG && console.log('element clicked:', event.target);
 
     var target = event.target;
     var eventIdFound = false;
@@ -181,10 +182,10 @@ document.onclick = function(event) {
         }
         target = target.parentElement;
     }
-    console.log('target:', target);
+    DEBUG && console.log('target:', target);
     if (eventIdFound) {
         var eventId = target.getAttribute('data-eventid');
-        console.log('event id found:', eventId);
+        DEBUG && console.log('event id found:', eventId);
 
         function modifyEventBubble(attempt) {
             attempt = attempt || 1;
@@ -194,7 +195,7 @@ document.onclick = function(event) {
             attempt += 1;
 
             var eventBubble = document.querySelector('#xDetDlg[data-eventid="' + eventId + '"]');
-            console.log('eventBubble:', eventBubble);
+            DEBUG && console.log('eventBubble:', eventBubble);
             if (!eventBubble) {
                 setTimeout(modifyEventBubble, 500, attempt);
                 return;
@@ -223,12 +224,12 @@ document.onclick = function(event) {
                 };
 
                 var editEventButton = document.querySelector('[aria-label="Edit event"]');
-                console.log('editEventButton:', editEventButton);
+                DEBUG && console.log('editEventButton:', editEventButton);
                 editEventButton.click();
             };
             insertButtons(newEventBubbleMetaItem, onclickAction, 'inside', lastNewEventBubbleMetaItem);
         }
         setTimeout(modifyEventBubble, 100);
     }
-    console.groupEnd();
+    DEBUG && console.groupEnd();
 };
