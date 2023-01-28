b2a0e0e0fb4c2753b77e7a17687fd5d6807eb115
Zach Borboa <zachborboa@gmail.com>
2023-01-27 18:44:31 -0800

    Move function outside

--- extensions/google_calendar_workflow/js/util.js
+++ extensions/google_calendar_workflow/js/util.js
@@ -13,54 +13,53 @@ function dispatchEvent(obj, event) {
     obj.dispatchEvent(evt);
 }
 
+function checkInputValue(input, value, resolve) {
+    // Focus input before checking if the input's value is the desired
+    // value. The value might be updated when another input loses focus.
+    input.focus();
+    DEBUG && console.log('input focused');
+
+    // Avoid resolving immediately even if input's value already matches.
+    // Input's value may be changed shortly after checking due to a delayed
+    // update.
+    setTimeout(() => {
+        if (input.value === value) {
+            DEBUG && console.log('value matches without updating');
+
+            setTimeout(() => {
+                DEBUG && console.log('checking value one more time');
+                if (input.value === value) {
+                    DEBUG && console.log('value matches without updating. resolving.');
+                    DEBUG && console.groupEnd();
+                    resolve();
+                } else {
+                    DEBUG && console.log('value no longer matches (desired value: "%s", current input value: "%s")', value, input.value);
+                    checkInputValue(input, value, resolve);
+                }
+            }, TIMEOUT_MS);
+        } else {
+            DEBUG && console.log('value does not match (desired value: "%s", current input value: "%s")', value, input.value);
+
+            input.value = value;
+            DEBUG && console.log('value set (current input value: "%s")', input.value);
+
+            setTimeout(() => {
+                dispatchEvent(input, 'input');
+                DEBUG && console.log('input event sent');
+
+                DEBUG && console.log('value is now "%s"', input.value);
+                checkInputValue(input, value, resolve);
+            }, TIMEOUT_MS);
+        }
+    }, TIMEOUT_MS);
+}
+
 function setInputValue(input, value) {
     DEBUG && console.group('setInputValue');
     // DEBUG && console.log('input:', input);
     DEBUG && console.log('value:', value);
     return new Promise((resolve, reject) => {
-
-        function checkInputValue() {
-            // Focus input before checking if the input's value is the desired
-            // value. The value might be updated when another input loses focus.
-            input.focus();
-            DEBUG && console.log('input focused');
-
-            // Avoid resolving immediately even if input's value already matches.
-            // Input's value may be changed shortly after checking due to a delayed
-            // update.
-            setTimeout(() => {
-                if (input.value === value) {
-                    DEBUG && console.log('value matches without updating');
-
-                    setTimeout(() => {
-                        DEBUG && console.log('checking value one more time');
-                        if (input.value === value) {
-                            DEBUG && console.log('value matches without updating. resolving.');
-                            DEBUG && console.groupEnd();
-                            resolve();
-                        } else {
-                            DEBUG && console.log('value no longer matches (desired value: "%s", current input value: "%s")', value, input.value);
-                            checkInputValue();
-                        }
-                    }, TIMEOUT_MS);
-                } else {
-                    DEBUG && console.log('value does not match (desired value: "%s", current input value: "%s")', value, input.value);
-
-                    input.value = value;
-                    DEBUG && console.log('value set (current input value: "%s")', input.value);
-
-                    setTimeout(() => {
-                        dispatchEvent(input, 'input');
-                        DEBUG && console.log('input event sent');
-
-                        DEBUG && console.log('value is now "%s"', input.value);
-                        checkInputValue();
-                    }, TIMEOUT_MS);
-                }
-            }, TIMEOUT_MS);
-        }
-
-        checkInputValue();
+        checkInputValue(input, value, resolve);
     });
 }
 
